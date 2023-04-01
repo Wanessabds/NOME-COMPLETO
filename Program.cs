8714fcc238c58f9f9a72a1785edaabc8c1fd2a53
Wanessabds <wanessa.nessap@gmail.com>
2023-03-31 21:50:30 -0300

    NOME-COMPLETO

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome, sobrenome;
+Console.Write("Escreva seu primeiro nome___: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Agora digite seu sobrenome___: ");
+sobrenome = Console.ReadLine()!;
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+Console.ForegroundColor = ConsoleColor.Cyan;
+
